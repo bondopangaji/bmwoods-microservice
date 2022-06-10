08c34f610767cb45dfb46148e85a7875dea6d508
bondopangaji <60285086+bondopangaji@users.noreply.github.com>
2022-06-10 13:00:26 +0700

    / move to correct package

--- notification-service/src/main/java/io/bondopangaji/notification/NotificationApplication.java
+++ notification-service/src/main/java/io/bondopangaji/notification/NotificationApplication.java
@@ -5,20 +5,29 @@
  * https://opensource.org/licenses/MIT
  */
 
-package io.bondopangaji;
+package io.bondopangaji.notification;
 
+import io.bondopangaji.messagequeue.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Bondo Pangaji
  */
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "io.bondopangaji.notification",
+                "io.bondopangaji.messagequeue"
+        }
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
 }
