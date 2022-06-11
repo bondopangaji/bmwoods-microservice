f09f66fb581f8984ebf0575cecdaadb482691a87
bondopangaji <60285086+bondopangaji@users.noreply.github.com>
2022-06-11 07:13:23 +0700

    + hexagonal architecture & package dependency testing

---
+++ product-service/src/test/java/io/bondopangaji/product/ArchitectureTest.java
@@ -0,0 +1,29 @@
+package io.bondopangaji.product;/*
+ * Copyright (c) 2022 Bondo Pangaji
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.library.Architectures;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Bondo Pangaji
+ */
+public class ArchitectureTest {
+    @Test
+    void validateHexagonalArchitecture() {
+        Architectures.onionArchitecture()
+                .domainModels("io.bondopangaji.product.domain..")
+                .domainServices("io.bondopangaji.product.application")
+                .applicationServices("io.bondopangaji.product.application..")
+                .adapter("api", "io.bondopangaji.product.adapter.api")
+                .adapter("persistence", "io.bondopangaji.product.adapter.persistence")
+                .adapter("rabbitmq", "io.bondopangaji.product.adapter.rabbitmq")
+                .adapter("spring", "io.bondopangaji.product.adapter.spring")
+                .check(new ClassFileImporter()
+                        .importPackages("io.bondopangaji.product"));
+    }
+}
---
+++ product-service/src/test/java/io/bondopangaji/product/PackageDependencyTest.java
@@ -0,0 +1,28 @@
+package io.bondopangaji.product;/*
+ * Copyright (c) 2022 Bondo Pangaji
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+/**
+ * @author Bondo Pangaji
+ */
+public class PackageDependencyTest {
+    @Test
+    void testPackageDependencies() {
+        noClasses()
+                .that()
+                .resideInAPackage("io.bondopangaji.product.domain..")
+                .should()
+                .dependOnClassesThat()
+                .resideInAnyPackage("io.bondopangaji.product.application..")
+                .check(new ClassFileImporter()
+                        .importPackages("io.bondopangaji.product.."));
+    }
+}
